a4773c819309c1175ec66522cd2fa389340ea048
YangelicaEspeleta <angelicaespeleta223@gmail.com>
2024-12-06 11:48:56 -0500

    Paginaestatica

---
+++ paginaestatica.js
@@ -0,0 +1,6 @@
+document.getElementById('ContacForm').addEventListener('submit',function(event){
+    event.preventDefault();//evitar que se recarge la pagina//
+    alert('Formulario enviado correctamente gracias por contactarnos..!')
+
+});
+
